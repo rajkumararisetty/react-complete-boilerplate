b44f0cf9abe33157dbe1de0013f5519776ae7a61
rajkumar.arisetty <rajkumar.arisetty@mindfiresolutions.com>
2019-03-23 01:30:58 +0530

    refactored

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -13,7 +13,9 @@ function setup() {
 
 describe('WelcomeMessage Test Suite', () => {
   it('Should have an image', () => {
-      fetchMock.post('http://stapi.co/api/v1/rest/character/search', { characters: [['React is great']] });
+    fetchMock.post('http://stapi.co/api/v1/rest/character/search', {
+      characters: [['React is great']],
+    });
 
     const { wrapper } = setup();
     expect(wrapper.find('img').exists()).toBe(true);
