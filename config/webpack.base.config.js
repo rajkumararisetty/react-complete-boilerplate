a8c097417aa77466e0a1bf732359d6643db20a59
rajkumar.arisetty <rajkumar.arisetty@mindfiresolutions.com>
2019-03-23 01:30:22 +0530

    Configured extra loaders for webpack css-scss-sass loaders and modified starter kit structure

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -5,6 +5,7 @@ const merge = require('webpack-merge');
 // Plugins
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const getCSSModuleLocalIdent = require('react-dev-utils/getCSSModuleLocalIdent');
 
 const APP_DIR = path.resolve(__dirname, '../src'); // <===== new stuff added here
 
@@ -23,18 +24,79 @@ module.exports = env => {
             },
           },
           {
-            test: /\.(scss|css)$/,
+            test: /\.(css)$/,
             use: [
               PLATFORM === 'production' ? MiniCssExtractPlugin.loader : 'style-loader',
-              'css-loader',
-              'sass-loader',
+              {
+                loader: require.resolve('css-loader'),
+                options: {
+                  importLoaders: 1,
+                  sourceMap: PLATFORM === 'production',
+                  modules: true,
+                  getLocalIdent: getCSSModuleLocalIdent,
+                },
+              },
+              {
+                loader: require.resolve('postcss-loader'),
+                options: {
+                  ident: 'postcss',
+                  plugins: () => [
+                    require('postcss-flexbugs-fixes'),
+                    require('postcss-preset-env')({
+                      autoprefixer: {
+                        flexbox: 'no-2009',
+                      },
+                      stage: 3,
+                    }),
+                  ],
+                  sourceMap: PLATFORM === 'production',
+                },
+              },
+            ],
+            sideEffects: true,
+          },
+          {
+            test: /\.(scss|sass)$/,
+            use: [
+              PLATFORM === 'production' ? MiniCssExtractPlugin.loader : 'style-loader',
+              {
+                loader: require.resolve('css-loader'),
+                options: {
+                  importLoaders: 1,
+                  sourceMap: PLATFORM === 'production',
+                  modules: true,
+                  getLocalIdent: getCSSModuleLocalIdent,
+                },
+              },
+              {
+                loader: require.resolve('postcss-loader'),
+                options: {
+                  ident: 'postcss',
+                  plugins: () => [
+                    require('postcss-flexbugs-fixes'),
+                    require('postcss-preset-env')({
+                      autoprefixer: {
+                        flexbox: 'no-2009',
+                      },
+                      stage: 3,
+                    }),
+                  ],
+                  sourceMap: PLATFORM === 'production',
+                },
+              },
+              {
+                loader: require.resolve('sass-loader'),
+                options: {
+                  sourceMap: PLATFORM === 'production',
+                },
+              },
             ],
           },
         ],
       },
       plugins: [
         new HtmlWebpackPlugin({
-          template: './src/index.html',
+          template: './public/index.html',
           filename: './index.html',
         }),
         new webpack.DefinePlugin({
